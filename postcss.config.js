d5c458caa681f508a3b39ea8a92176db3500d301
Galushko Evgeniy <yudginchik@gmail.com>
2025-05-22 20:42:53 +0300

    Styling

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// };
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
+import StatusLabelProps from './components/status-label.jsx';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-3xl font-bold underline">Home page</h1>
+      <StatusLabelProps status={'active'}>Active</StatusLabelProps>
+      <StatusLabelProps status={'notActive'}>Not active</StatusLabelProps>
+      <StatusLabelProps status={'pending'}>Pending</StatusLabelProps>
+      <StatusLabelProps status={'suspended'}>Suspended</StatusLabelProps>
     </main>
   );
 }
