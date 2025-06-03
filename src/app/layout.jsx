f19770eb081ca8b806a794f1eb102cb79f1a7481
Galushko Evgeniy <yudginchik@gmail.com>
2025-06-03 11:54:06 +0300

    6-1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
 import Sidebar from './components/sidebar.jsx';
+import Providers from './components/providers.jsx';
 
 const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
